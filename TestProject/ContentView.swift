6115cbb4940725ac9bf13f0ce2fa845c2a1cf143
Harold Tomlinson <haroldt@creacom.ca>
2024-03-03 14:08:29 -0500

    Initial Commit

---
+++ TestProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestProject
+//
+//  Created by Harold Tomlinson on 2024-03-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Harold Tomlinson on 2024-03-03.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
